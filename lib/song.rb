1976cb6b120a82ab3981e3e74048cd7eb1606589
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-10 15:57:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,5 +6,14 @@ class Song
     @artist = artist
     @genre = genre
   end
-  
+
+  @@song_count = 0 
+
+  def self.count
+    @@song_count
+  end 
+
+  def initialize
+    @@song_count += 1
+  end
 end
